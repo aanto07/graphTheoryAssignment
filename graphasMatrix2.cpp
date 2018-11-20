bab21a1b79e8dec0885f0f8cd175b236fc70f0c9
Alan Anto <alananto3537@gmail.com>
2018-11-20 10:53:07 +0530

    Updated graphasMatrix2.cpp

--- graphasMatrix2.cpp
+++ graphasMatrix2.cpp
@@ -9,13 +9,13 @@ void printMatrix(int mat[][20],int n)
 	cout<<"\n ******** Graph as Adjacency Matrix ******* \n \n";
 	int i,j;
 	cout<<"\n\n"<<setw(4)<<"";
-	for(i = 0; i < n; i++)
-        cout<<setw(3)<<"("<<i+1<<")";
+	for(i = 1; i <=n; i++)
+        cout<<setw(3)<<"("<<i<<")";
     cout<<"\n\n";
-    for(i = 0; i < n; i++)
+    for(i = 1; i <= n; i++)
     {
-        cout<<setw(3)<<"("<<i+1<<")";
-        for(j = 0; j < n; j++)
+        cout<<setw(3)<<"("<<i<<")";
+        for(j = 1; j <=n; j++)
         {
             cout<<setw(4)<<mat[i][j];
         }
@@ -37,15 +37,20 @@ int main()
 
   int matrix[20][20];
 
-  for(int i=0;i<n;i++)
+  for(int i=1;i<=n;i++)
   {
-  	for(int j=i;j<n;j++)
+  	for(int j=i;j<=n;j++)
   	{
   		if(i!=j)
   		{
-  			cout<<"Is edge "<<i+1<<" connected with edge "<<j+1<<" (y/n) \n";
+  			cout<<"Is edge "<<i<<" connected with edge "<<j<<" (y/n) \n";
   			cin>>a;
-  			matrix[i][j] = (a=='Y'&&a=='y')?0:1; 
+  			if(a=='Y'|| a=='y')
+        {
+          matrix[i][j] =1;
+        }
+        else
+          matrix[i][j] = 0;
 
   			matrix[j][i] = matrix[i][j];
   		}
